4b89798d1641846548cb354e05012a3ba9364023
Norman Sander <norman@nsander.de>
2015-09-12 02:17:59 +0200

    Second vc

---
+++ Eggy/InfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InfoViewController.swift
+//  Eggy
+//
+//  Created by Norman Sander on 12.09.15.
+//  Copyright © 2015 Norman Sander. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoViewController: UIViewController {
+
+}
+
