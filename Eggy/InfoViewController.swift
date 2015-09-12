4d3e74ef3f077e5e446571ffc1721279dc329b4d
Norman Sander <norman@nsander.de>
2015-09-12 02:45:25 +0200

    Tableview datasource

--- Eggy/InfoViewController.swift
+++ Eggy/InfoViewController.swift
@@ -9,7 +9,36 @@
 import Foundation
 import UIKit
 
-class InfoViewController: UIViewController {
+class InfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    var items: [String] = ["We", "Heart", "Swift"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "InfoCell")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("InfoCell")!
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
-
